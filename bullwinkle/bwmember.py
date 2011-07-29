4efd918c5734df5edc421897075bae9f0c54ac7e
Rich Harkins <rich.harkins@gmail.com>
2011-07-29 16:49:51 -0400

    Removed false unit testing from member

--- bullwinkle/bwmember.py
+++ bullwinkle/bwmember.py
@@ -1,11 +1,6 @@
 '''
 bwmember -- Easy to build type-safe propreties
 
->>> class MyClass(BWObject):
-...     x = member(int)
-...
->>> c = MyClass(x=5)
->>> c.x
 '''
 
 from bwobject import BWObject
