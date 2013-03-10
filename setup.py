14d84811cdc18a31b3cbdd96a898474872a5333a
Rich Harkins <rich.harkins@gmail.com>
2013-03-10 03:14:40 -0400

    Added missing lib

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     author = AUTHOR,
     author_email = AUTHOR_EMAIL,
     description = 'Python tools inspired by Perl::Moose',
-    licesne = LICENSE,
+    license = LICENSE,
     keywords = 'Moose OOP super',
     url = 'http://code.google.com/p/bullwinkle',
     packages = ['bw', 'bw.util'],
